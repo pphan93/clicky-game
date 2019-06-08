c45d2c2de54286e3fcdb5534bfce1fd2d8eec3c8
Phuoc phan <phuocphan12@gmail.com>
2019-06-08 00:10:46 -0400

    p

---
+++ src/components/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "../styles/Header.css";
+
+function Image() {
+  return (
+      <div>
+<div role="img" aria-label="click item" class="click-item" style="background-image: url(&quot;/assets/images/goldenford.png&quot;);"></div>
+</div>
+  
+  );
+  
+}
+
+export default Image;
