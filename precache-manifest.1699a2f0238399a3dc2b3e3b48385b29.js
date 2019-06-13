fabde45954f2dddebe31345a81593f22db5841a0
Phuoc phan <phuocphan12@gmail.com>
2019-06-12 22:43:41 -0400

    Updates

---
+++ precache-manifest.1699a2f0238399a3dc2b3e3b48385b29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba639395b00046c9ccf2ca29483170b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "50cd734aefdb4227745e",
+    "url": "/clicky-game/static/css/main.ff17f998.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "50cd734aefdb4227745e",
+    "url": "/clicky-game/static/js/main.d7a9628b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
